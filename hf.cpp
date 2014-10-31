eac0425bff352d48490143894f43af7bdbfba179
Orbán István <firstvan@live.com>
2014-10-31 21:24:43 +0100

    second homework do the PLU, and A-c

--- hf.cpp
+++ hf.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char ** argv){
     int taskNumber;
-        scanf("%d", &taskNumber);
+    scanf("%d", &taskNumber);
 
     int taskProcess;
     for(taskProcess = 0; taskProcess < taskNumber; taskProcess++){
@@ -12,7 +12,7 @@ int main(int argc, char ** argv){
         int sing = 0;
         double temp;
         int dim;
-        
+
         scanf("%d", &dim);
 
         double x[dim];
@@ -23,12 +23,12 @@ int main(int argc, char ** argv){
             for(j = 0; j < dim; j++)
                 scanf("%lf", &matrix[i][j]);
 
- 		double b[dim];
+        double b[dim];
         for(i = 0; i < dim; i++)
             scanf("%lf", &b[i]);
-        
+
         double p[dim];
-        
+
         for(i = 0; i < dim; i++){
             p[i] = i;
         }
@@ -36,11 +36,11 @@ int main(int argc, char ** argv){
         int k;
 
         for(k = 0; k < dim - 1; k++){
-            
+
             int maxRow = k;
             for(i = k+1; i < dim; i++){
                 if(fabs(matrix[i][k]) > fabs(matrix[maxRow][k])){
-                   maxRow = i;
+                    maxRow = i;
                 }
             }     //jó       
 
@@ -50,9 +50,9 @@ int main(int argc, char ** argv){
                     matrix[k][i] = matrix[maxRow][i];
                     matrix[maxRow][i] = temp;
                 }
-                
+
                 csere++;
-                    
+
                 p[k] = maxRow;
                 p[maxRow] = k;
 
@@ -60,71 +60,71 @@ int main(int argc, char ** argv){
 
             if(fabs(matrix[k][k]) < 1e-15)
                 sing = 1;
-            
-               
+
+
             for(i = k+1; i < dim; i++){
                 matrix[i][k] = matrix[i][k]/matrix[k][k];
-                
+
                 for(j = k+1; j < dim; j++){
-	                matrix[i][j] = matrix[i][j] - (matrix[i][k] * matrix[k][j]);     
+                    matrix[i][j] = matrix[i][j] - (matrix[i][k] * matrix[k][j]);     
                 }
-                
-          	}
+
+            }
         }
-        
+
         if(fabs(matrix[dim-1][dim-1]) < 1e-15)
-                sing = 1;
+            sing = 1;
         else{
-        
+
             double n_b[dim];
 
             for(i = 0; i < dim; i++){
                 int intTemp = p[i];
                 n_b[i] = b[intTemp];
             }
-                    
+
             temp = 0;
-                  
+
             //b vektor már nem kell így felhasználom
             for(i = 0; i < dim; i++){
-            	temp = 0;  
+                temp = 0;  
                 for(j = 0; j < i; j++)
                     temp += (matrix[i][j] * b[j]);    
-                      
+
                 b[i] = n_b[i] - temp;
             }
 
             for(i = dim - 1; i >= 0; i--){
-            	temp = 0;
+                temp = 0;
                 for(j = i+1; j < dim; j++)
- 	                temp += (matrix[i][j] * x[j]);
-                
+                    temp += (matrix[i][j] * x[j]);
 
-            	x[i] = (b[i] - temp);
-            	if(fabs(x[i]) > 1e-15)
-            		x[i] = x[i] / matrix[i][i];
+
+                x[i] = (b[i] - temp);
+                if(fabs(x[i]) > 1e-15)
+                    x[i] = x[i] / matrix[i][i];
             }                     
-         }
+        }
 
-         if(sing == 1)
-             printf("singularis");
-         else
-         {
+        if(sing == 1)
+            printf("singularis");
+        else
+        {
             double det = 1;
-            
+
             for(i = 0; i < dim; i++)
                 det *= matrix[i][i];
 
             if(csere % 2 == 1)
-            	det *= -1;
-            
+                det *= -1;
+
             printf("%lf ",det);
 
             for(i = 0; i < dim; i++)
                 printf("%lf ", x[i]);
-             
-         }
 
-     printf("\n");   
+        }
+
+        printf("\n");   
     }
 }
--- hf2.cpp
+++ hf2.cpp
@@ -1,12 +1,160 @@
 #include <stdio.h>
+#include <math.h>
 
+
+void initMatrix(double *** A, const int & size){
+
+    int j;
+
+    //Matrix init
+    *A = new double*[size];
+    for (j = 0; j < size; ++j)
+    {
+        (*A)[j] = new double[size];
+    }
+
+}
+
+void deleteMatrix(double *** A, const int &size){
+
+    int j;
+
+    //Delete matrix
+    for (j = 0; j < size; ++j)
+    {
+        delete[] (*A)[j];
+    }
+
+    delete[] (*A);
+}
+
+void initVec(double ** A,const int& size){
+    *A = new double[size];
+}
+
+void deleteVec(double ** A,const int& size){
+    delete[] *A;
+}
+
+int matrixPLU(double *** A, double ** P,const int& size){
+    int k,i,j;
+    double temp;
+
+    for (i = 0; i < size; ++i)
+    {
+        (*P)[i] = i;
+    }
+
+    for (k = 0; i < size - 1; ++i)
+    {
+        int maxRow = k;
+
+        for (i = k+1; i < size; ++i)
+            if(fabs((*A)[i][k]) > fabs((*A)[maxRow][k]))
+                maxRow = i;
+
+        if(maxRow != k){
+            for (i = 0; i < size; ++i)
+            {
+                temp = (*A)[k][i];
+                (*A)[k][i] = (*A)[maxRow][i];
+                (*A)[maxRow][i] = temp;
+            }
+
+            int tempI = (*P)[k];
+            (*P)[k] = (*P)[maxRow];
+            (*P)[maxRow] = tempI;
+        }
+
+        if(fabs((*A)[k][k]) < 1e-15)
+            return 1;
+
+        for (i = k + 1; i < size; ++i)
+        {
+            (*A)[i][k] /= (*A)[k][k];
+
+            for (j = k + 1; j < size; ++j)
+            {
+                (*A)[i][j] -= (*A)[i][k] * (*A)[k][j];
+            }
+        }
+
+    }
+
+    return 0;
+}
 
 int main(){
+    int N;
+
+    scanf("%d", &N);
+
+    int i;
+    for (i = 0; i < N; ++i)
+    {
+        int j, k;              //iteration
+        int nMatrixM;       //Matrix size
+        scanf("%d", &nMatrixM);
+
+        //Matrix init
+        double ** matrix;
+        initMatrix(&matrix, nMatrixM);
 
 
+        //add to matrix elements
+        for (j = 0; j < nMatrixM; ++j)
+        {
+            for (k = 0; k < nMatrixM; ++k)
+            {
+                scanf("%lf", &matrix[j][k]);
+            }
+        }
 
+        int m;              //taskNumber
+        scanf("%d", &m);
 
+        for (j = 0; j < m; ++j)
+        {
+            double c;        //shifting
+            scanf("%lf", &c);
 
+            int maxit;       //maximal of iteration
+            scanf("%d", &maxit);
 
-return 0;
+            double epsilon;
+            scanf("%lf", &epsilon);
+
+            double * y;
+            initVec(&y, nMatrixM);
+
+            for (k = 0; k < nMatrixM; ++k)
+            {
+                scanf("%lf", &y[j]);
+            }
+
+            double * p;
+            initVec(&p, nMatrixM);
+
+            int singular = matrixPLU(&matrix, &p, nMatrixM);
+
+            if(!singular){
+                printf("singular matrix, c=%.8lf", c);
+                deleteVec(&p, nMatrixM);
+                deleteVec(&y, nMatrixM);
+                break;
+            }
+
+            for (k = 0; k < nMatrixM; ++k)
+            {
+                matrix[k][k] -= c;
+            }
+
+            deleteVec(&p, nMatrixM);
+            deleteVec(&y, nMatrixM);
+        }
+
+        deleteMatrix(&matrix, nMatrixM);
+    }
+
+    return 0;
 }
